4e7833ae663d434634374dc042a68824b0e3f4da
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-02-27 15:27:47 -0600

    moved extend() from a module to raw source

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-GLOBAL.extend = require("extend");
+require("./extensions/utilities");
 require("./extensions/string");
 require("./extensions/array");
 
