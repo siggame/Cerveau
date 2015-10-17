399e0911eeebb3ea0949a71243ab256d147e0ff8
epicpants <tjrg88@mst.edu>
2015-10-17 11:59:39 -0500

    Refactored and fixed variable declaration in WeatherStation.rotate

--- games/anarchy/weatherStation.js
+++ games/anarchy/weatherStation.js
@@ -92,27 +92,27 @@ var WeatherStation = Class(Building, {
     rotate: function(player, counterclockwise, asyncReturn) {
         // <<-- Creer-Merge: rotate -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
 
-        game = this.game;
+        var game = this.game;
 
         // check if the weatherstation is alive
         if(this.health <= 0) {
-            return game.logicError(false, "tried to bribe a burned down WeatherStation")
+            return game.logicError(false, "tried to bribe a burned down WeatherStation");
         }
 
         // check if the player owns the building
         if(this.owner !== player) {
-            return game.logicError(false, "tried to use an enemy's WeatherStation.rotate")
+            return game.logicError(false, "tried to use an enemy's WeatherStation.rotate");
         }
 
         // check if the player has enough bribes remaining
         if(this.owner.bribesRemaining <= 0) {
 
-            return game.logicError(false, "tried to bribe with no bribesRemaining") 
+            return game.logicError(false, "tried to bribe with no bribesRemaining");
         }
 
         // check if the building has already been bribed
         if(this.bribed) {
-            return game.logicError(false, "this building has already been bribed")
+            return game.logicError(false, "this building has already been bribed");
         }
 
         this.bribed = true;
@@ -125,24 +125,24 @@ var WeatherStation = Class(Building, {
                 game.nextForecast.direction = "east";
             }
         }
-        else if (game.nextForecast.direction === "east") {
-            if (counterclockwise) {
+        else if(game.nextForecast.direction === "east") {
+            if(counterclockwise) {
                 game.nextForecast.direction = "north";
             }
             else {
                 game.nextForecast.direction = "south";
             }
         }
-        else if (game.nextForecast.direction === "south") {
-            if (counterclockwise) {
+        else if(game.nextForecast.direction === "south") {
+            if(counterclockwise) {
                 game.nextForecast.direction = "east";
             }
             else {
                 game.nextForecast.direction = "west";
             }
         }
-        else if (game.nextForecast.direction === "west") {
-            if (counterclockwise) {
+        else if(game.nextForecast.direction === "west") {
+            if(counterclockwise) {
                 game.nextForecast.direction = "south";
             }
             else {
