0af53de05febdd41062e7eb1effbde6f3c0c1ead
Jacob Fischer <jfischer@r99acm.device.mst.edu>
2015-11-14 14:18:52 -0600

    corss origin fun times

--- website/api.js
+++ website/api.js
@@ -51,6 +51,9 @@ module.exports = function(args) {
             }
         }
 
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
         res.json(response);
     });
 
