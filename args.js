b68241b343c2a3a7a33dc1573ef479443afedbe3
Jacob Fischer <jacob.t.fischer@gmail.com>
2016-09-26 15:29:11 -0500

    Add config for saved cli args

--- args.js
+++ args.js
@@ -1,12 +1,12 @@
 var ArgumentParser = require('argparse').ArgumentParser;
 var parser = new ArgumentParser({description: "Run the JavaScript client with options to connect to a game server. Must provide a game name to play."});
 var parserArgs = [
-    [["-p", "--port"], {action: "store", dest: "port", defaultValue: 0, help: "port offset for the default port values"}],
+    [["--port-offset"], {action: "store", dest: "portOffset", defaultValue: 0, help: "port offset for the default port values"}],
     [["--tcp-port"], {action: "store", dest: "tcpPort", defaultValue: 3000, help: "the port that TCP clients should connect through"}],
     [["--http-port"], {action: "store", dest: "httpPort", defaultValue: 3080, help: "the port that the webinterface should be accessed through"}],
     [["--ws-port"], {action: "store", dest: "wsPort", defaultValue: 3088, help: "the port that WebSocket clients should connect though"}],
-    [["-H", "--host"], {action: "store", dest: "host", defaultValue: "127.0.0.1", help: "the host that this should run on"}],
-    [["-t", "--title"], {action: "store", dest: "title", defaultValue: "Cerveau", help: "the title of this game sever for the web interface"}],
+    [["--host"], {action: "store", dest: "host", defaultValue: "127.0.0.1", help: "the host that this should run on"}],
+    [["--title"], {action: "store", dest: "title", defaultValue: "Cerveau", help: "the title of this game sever for the web interface"}],
     [["--authenticate"], {action: "storeTrue", dest: "authenticate", help: "forces clients to authenticate against the authentication server"}],
     [["--profile"], {action: "storeTrue", dest: "profile", help: "run the v8 profilers against threaded game sessions"}],
     [["--log"], {action: "storeTrue", dest: "log", help: "store all logged strings to text files in output/logs/"}],
@@ -26,7 +26,15 @@ for(var i = 0; i < parserArgs.length; i++) {
     parser.addArgument.apply(parser, parserArgs[i]);
 }
 
-var args = parser.parseArgs();
+// Parse config.json and make it into cli args
+var config = require("./config.json");
+for(var key in config) {
+    if(config.hasOwnProperty(key)) {
+        process.argv.push("--" + key, config[key]);
+    }
+}
+
+var args = parser.parseArgs(process.argv.slice(2)); // first two args are `node main.js`, with the full math to each
 
 if(args.profile) {
     try {
@@ -51,8 +59,8 @@ if(args.chesser) {
     args.chesser += "?file={gamelogFilename}";
 }
 
-if(!isNaN(args.port)) {
-    var portOffset = parseInt(args.port);
+if(!isNaN(args.portOffset)) {
+    var portOffset = parseInt(args.portOffset);
     args.tcpPort += portOffset;
     args.httpPort += portOffset;
     args.wsPort += portOffset;
