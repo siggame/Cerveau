7c1634d70dace567050b69f7b99c36a1651d706a
jssfg8 <jssfg8@mst.edu>
2017-10-12 19:04:57 -0500

    Fixed init, used data

--- games/catastrophe/unit.js
+++ games/catastrophe/unit.js
@@ -108,6 +108,12 @@ let Unit = Class(GameObject, {
         //<<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
 
         this.energy = 100;
+        this.job = data.job || this.game.jobs[0];
+        this.moves = this.job.moves;
+        this.owner = data.owner || null;
+        this.tile = data.tile || null;
+        this.turnsToDie = -1;
+        
         // Soldiers are in their own squad, and other units, while not in squads, should be in their own squad for ease of use.
         this.calculateSquad();
 
