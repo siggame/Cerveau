c29a026a726cc49438ef4c60260fb837accf5fe8
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-03-27 16:27:40 -0500

    update clients index code

--- src/core/clients/index.ts
+++ src/core/clients/index.ts
@@ -1,15 +1,25 @@
-// All the client protocals we support
+// All the client protocols we support
 // They should all inherit from the Client base class
 
-var clientClasses = {
-    WS: require("./wsClient"),
-    TCP: require("./tcpClient"),
-};
+export * from "./base-client";
+export * from "./tcp-client";
+export * from "./ws-client";
 
-for(var key in clientClasses) {
-    if(clientClasses.hasOwnProperty(key)) {
-        clientClasses[key].prototype.connectionType = key;
+export * from "./events-client";
+export * from "./events-server";
+
+import { BaseClient } from "./base-client";
+import { TCPClient } from "./tcp-client";
+import { WSClient } from "./ws-client";
+
+export function getClientByType(type: "tcp"): typeof TCPClient;
+export function getClientByType(type: "ws"): typeof WSClient;
+export function getClientByType(type: string): typeof BaseClient | undefined;
+export function getClientByType(type: string): typeof BaseClient | undefined {
+    switch (type) {
+        case "tcp":
+            return TCPClient;
+        case "ws":
+            return WSClient;
     }
 }
-
-module.exports = clientClasses;
