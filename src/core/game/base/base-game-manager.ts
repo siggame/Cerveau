bbc810fba5bb173e8ac9424e131da1aea2b22ce0
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-03-27 16:34:29 -0500

    fix files from ts migration

--- src/core/game/base/base-game-manager.ts
+++ src/core/game/base/base-game-manager.ts
@@ -1,147 +1,280 @@
-var fs = require("fs");
-var Class = require(__basedir + "/utilities/class");
-var Errors = require(__basedir + "/gameplay/errors");
-var log = require(__basedir + "gameplay/log");
-var serializer = require(__basedir + "gameplay/serializer");
-var DeltaMergeableArray = require(__basedir + "gameplay/shared/deltaMergeableArray");
-var DeltaMergeableDictionary = require(__basedir + "gameplay/shared/deltaMergeableDictionary");
+import { BaseClient } from "src/core/clients/";
+import { DeltaMergeable } from "src/core/game";
+import { RandomNumberGenerator } from "src/core/game/random-number-generator";
+import { Event } from "ts-typed-events";
+import { BaseGame } from "./base-game";
+import { IBaseGameNamespace } from "./base-game-namespace";
+import { BaseGameObject } from "./base-game-object";
+import { BaseGameObjectFactory } from "./base-game-object-factory";
+import { IBaseGameSettings } from "./base-game-settings";
+import { IBasePlayer } from "./base-player";
 
 /**
- * A collection of static functions to sanitize inputs from AI clients for the ${game_name} Game.
+ * the base game plugin new games should inherit from.
  */
-var GameManager = Class({
-    init: function(game) {
-        this._game = game;
-    },
+export class BaseGameManager {
+    public static get gameName(): string {
+        return "BaseGame";
+    }
+
+    public static get requiredNumberOfPlayers(): number {
+        return 0;
+    }
+
+    public static get aliases(): string[] {
+        return [];
+    }
 
     /**
-     * Sanitizes a variable to some type
-     * @param {string} type - what type to turn sanitizing into
-     * @param {*} sanitizing - what to cast from
-     * @returns {*} sanitizing converted to type
+     * A factory that can create game objects for this manager's game.
      */
-    sanitizeType: function(type, sanitizing) {
-        switch(type) {
-            case "int":
-                return serializer.defaultInteger(sanitizing);
-            case "float":
-                return serializer.defaultNumber(sanitizing);
-            case "boolean":
-                return serializer.defaultBoolean(sanitizing);
-            case "string":
-                return serializer.defaultString(sanitizing);
-            case "array":
-                if(DeltaMergeableArray.isInstance(sanitizing)) {
-                    return sanitizing;
-                }
-                return null;
-            case "dictionary":
-                if(DeltaMergeableDictionary.isInstance(sanitizing)) {
-                    return sanitizing;
-                }
-                return null;
-            default:
-                if(type[0] === type[0].toUpperCase()) { // then it is a GameObject
-                    var gameObjectClass = this._game.classes[type];
-                    return gameObjectClass.isInstance(sanitizing) ? sanitizing : null;
-                }
-                return null;
+    public readonly create: BaseGameObjectFactory;
+
+    /**
+     * The game this GameManager is managing
+     */
+    public readonly game: BaseGame;
+
+    /**
+     * This is aa Pseudo-Random Number Generator that is synced with the game
+     * clients and game log to perform random but tracked events from a stored
+     * seed.
+     */
+    public readonly random: RandomNumberGenerator;
+
+    private isOver = false;
+    private nextGameObjectID = 0;
+
+    /** Mapping of a player to their client */
+    private playerToClient = new Map<IBasePlayer, BaseClient>();
+
+    constructor(
+        private readonly namespace: IBaseGameNamespace,
+        settings: IBaseGameSettings,
+        clients: BaseClient[],
+        private gameOverCallback: () => void,
+    ) {
+        if (!settings.randomSeed) {
+            // tslint:disable-next-line:no-math-random - because this is the only place we use old random
+            settings.randomSeed = Math.random().toString(36).substring(2);
         }
-    },
+        this.random = new RandomNumberGenerator(settings.randomSeed);
+
+        const invalidateRun = (player: IBasePlayer,
+                               gameObject: BaseGameObject,
+                               functionName: string,
+                               args: Map<string, any>,
+        ) => {
+            return this.invalidateRun(player, gameObject, functionName, args);
+        };
+
+        for (const client of clients) {
+            client.aiManager!.invalidateRun = invalidateRun;
+        }
+
+        const gameCreated = new Event<{game: BaseGame, gameObjects: DeltaMergeable}>();
+
+        gameCreated.once(({ game }) => {
+            (this.game as any) = game; // will happen synchronously, but TS doesn't know
+        });
+
+        // TODO this needs to be ready BEFORE the game...
+        this.create = new this.namespace.GameObjectFactory(
+            this.namespace,
+            this.generateNextGameObjectID,
+            gameCreated,
+        );
+
+        this.game = new this.namespace.Game(settings, {
+            namespace,
+            clients,
+            manager: this,
+            playerIDs: clients.map(() => this.generateNextGameObjectID()),
+            schema: this.namespace.gameObjectsSchema.Game,
+            gameCreated,
+        });
+
+        for (const client of clients) {
+            client.events.disconnected.once(() => this.playerDisconnected(client.player!));
+            this.playerToClient.set(client.player!, client);
+        }
+    }
 
     /**
-     * Sanitized the arguments for an order/run command
-     *
-     * @param {Array.<Object>} argsStructure - array of args information
-     * @param {Array} untreatedArgs - args as sent that have not been sanitized
-     * @param {Object} [args] - the arguments as a key value dictionary
-     * @returns {Array} a new array of sanitized args
+     * declares some player(s) as having lost, and assumes when a player looses
+     * the rest could still be competing to win.
+     * @param reason the reason they lost
+     * @param loser the player that lost the game
+     * @param {Player} losers additional player(s) that lost the game
      */
-    _sanitizeArgs: function(argsStructure, untreatedArgs, args) {
-        var sanitized = [];
-        for(var i = 0; i < argsStructure.length; i++) {
-            var arg = argsStructure[i];
-            var val = untreatedArgs[arg.name] || untreatedArgs[i];
+    public declareLoser(reason: string, loser: IBasePlayer, ...losers: IBasePlayer[]): void {
+        losers.push(loser);
+        for (const player of losers) {
+            this.setPlayerLost(player, reason);
+        }
+    }
 
-            sanitized[i] = this._sanitizeValue(val, arg);
-            if(args !== undefined) {
-                args[arg.name] = val;
+    /**
+     * declares some player(s) as having won, and assumes when a player wins the
+     * rest have lost if they have not won already
+     * @param reason the reason they won
+     * @param winner the winner of the game
+     * @param {Player} winners additional the player(s) that won the game
+     */
+    public declareWinner(reason: string, winner: IBasePlayer, ...winners: IBasePlayer[]): void {
+        winners.push(winner);
+        for (const player of winners) {
+            player.lost = false;
+            player.reasonLost = "";
+            player.won = true;
+            player.reasonWon = reason;
+        }
+
+        this.checkForGameOver();
+    }
+
+    /**
+     * End the game via coin flip (1 random winner, the rest lose)
+     *
+     * @param {string} [reason="Draw"] - optional reason why win via coin flip is happening
+     */
+    public makePlayerWinViaCoinFlip(reason: string = "Draw"): void {
+        // Win via coin flip - if we got here no player won via game rules.
+        // They probably played identically to each other.
+        const players = this.game!.players.filter((p) => !p.won && !p.lost);
+
+        const winnerIndex = this.random.int(players.length);
+        for (let i = 0; i < players.length; i++) {
+            if (i === winnerIndex) {
+                this.declareWinner(`${reason} - Won via coin flip.`, players[i]);
+            }
+            else {
+                this.declareLoser(`${reason} - Lost via coin flip.`, players[i]);
             }
         }
-
-        return sanitized;
-    },
+    }
 
     /**
-     * Sanitizes a single value
-     *
-     * @param {*} val - unsanitized value
-     * @param {Object} arg - information about what the value should look like
-     * @returns {*} val, sanitized
+     * You **MUST** call this to let everything know the game is over and
+     * all the clients should be notified
+     * @param reason the reason the game is over to set for any players that
+     * have not already won or lost the game
      */
-    _sanitizeValue: function(val, arg) {
-        return this.sanitizeType(arg.type.name, val === undefined ? arg.defaultValue : val);
-    },
-
-    /**
-     * Sanatizes an AI's incoming arguments to a run event
-     *
-     * @param {Function} runFunction - the ai run function
-     * @param {Object} kwargs - key word formatted args (not array in order)
-     * @param {Object} args - key/value arguments object sanitized
-     * @returns {Array} sanitized args for the run
-     */
-    sanitizeRun: function(runFunction, kwargs, args) {
-        return this._sanitizeArgs(runFunction.cerveau.args, kwargs, args);
-    },
-
-    /**
-     * Sanatizes the servers return value from running game logic in response to an AI's run event
-     *
-     * @param {Function} ranFunction - the server-side (this) ran function callback
-     * @param {*} runReturned - unsanitized value from the server it returned from the run, to be sent to the ai
-     * @returns {*} santized return value from the ran
-     */
-    sanitizeRan: function(ranFunction, runReturned) {
-        var returns = ranFunction.cerveau.returns;
-        if(returns !== undefined) { // meaning there is an expected return value, then sanitize it
-            return this._sanitizeValue(runReturned, returns);
+    public endGame(reason: string = "Draw"): void {
+        const playingPlayers = this.game.players.filter((p) => !p.won && !p.lost);
+        if (playingPlayers.length > 0) {
+            this.declareLoser(reason, playingPlayers.pop()!, ...playingPlayers);
         }
-    },
+
+        this.isOver = true;
+        this.gameOverCallback();
+    }
 
     /**
-     * Sanatizes the arguments the server will be sending to an order for the upcoming AI's order event
-     *
-     * @param {Object} order - the order to sanitize
+     * Checks if the game is over
+     * @returns {boolean} true if the game is over, false otherwise
      */
-    sanitizeOrder: function(order) {
-        var structure = this._game["aiFinished" + order.name.upcaseFirst()].cerveau;
-        order.args = this._sanitizeArgs(structure.args, order.args);
-    },
+    public isGameOver(): boolean {
+        return this.isOver;
+    }
 
     /**
-     * Sanitizes what the AI returned from their order
-     *
-     * @param {Object} order - the order the ai finished
-     * @param {*} orderReturned - the return value from the ai of what it returned from the order
-     * @returns {*} sanitized version of orderReturned
+     * Generates a new id string for a new game object
+     * @returns a string for the new id. **must be unique**s
      */
-    sanitizeFinished: function(order, orderReturned) {
-        var structure = this._game["aiFinished" + order.name.upcaseFirst()].cerveau;
-        if(structure.returns !== undefined) {
-            return this._sanitizeValue(orderReturned, structure.returns);
+    protected generateNextGameObjectID = () => {
+        // returns this._nextGameObjectID then increments by 1 (that's how post++ works FYI)
+        return String(this.nextGameObjectID++);
+    }
+
+    /**
+     * Invoked any time an AI wants to run some game object function.
+     * If a string is returned that is the reason why it is invalid.
+     * @param player the player invoking the function
+     * @param gameObject the game object being invoked on
+     * @param functionName the string name of the function
+     * @param args the key/value pair args to the function
+     * @returns a string if the run is invalid, nothing if valid
+     */
+    protected invalidateRun(
+        player: IBasePlayer,
+        gameObject: BaseGameObject,
+        functionName: string,
+        args: Map<string, any>,
+    ): string | undefined {
+        // all runs are valid by default
+        return undefined;
+    }
+
+    /**
+     * Called when a client disconnected to remove the client from the game and
+     * check if they have a player and if removing them alters the game
+     * @param {Player} player - the player whose client disconnected
+     */
+    private playerDisconnected(player: IBasePlayer): void {
+        if (player && !this.isOver) {
+            this.declareLoser("Disconnected during gameplay.", player);
+
+            const losers = this.game.players.filter((p) => p.lost);
+            if (losers.length === this.game.players.length - 1) {
+                // then only one player is left in the game, he wins!
+
+                // and this is them!
+                const winner = this.game.players.find((p) => !p.lost);
+
+                const allLosersDisconnected = losers
+                    .filter(
+                        (p) => this.playerToClient.get(p)!.hasDisconnected(),
+                    )
+                    .length === losers.length;
+
+                const allLosersTimedOut = losers
+                    .filter(
+                        (p) => this.playerToClient.get(p)!.hasTimedOut(),
+                    )
+                    .length === losers.length;
+
+                let reasonWon = "All other players lost.";
+                if (allLosersDisconnected) {
+                    reasonWon = "All other players disconnected.";
+                }
+                if (allLosersTimedOut) {
+                    reasonWon = "All other players timed out.";
+                }
+
+                if (winner) {
+                    this.declareWinner(reasonWon, winner);
+                }
+            }
         }
-    },
+    }
 
     /**
-     * Checks if a cerveau callback is a secret (not codegened)
-     *
-     * @param {Function} callback - callback to check (not invoke)
-     * @returns {Boolean} true if it is a secret, false otherwise
+     * The player that lost the game
+     * @param loser the loser
+     * @param reason the reason they lost
      */
-    isSecret: function(callback) {
-        return Boolean(callback.cerveau.isSecret);
-    },
-});
+    private setPlayerLost(loser: IBasePlayer, reason: string): void {
+        loser.lost = true;
+        loser.reasonLost = reason;
+        loser.won = false;
+        loser.reasonWon = "";
+    }
 
-module.exports = GameManager;
+    /**
+     * Does a basic check if this game is over because there is a winner (all
+     * other players have lost). For game logic related winner checking you
+     * should write your own checkForWinner() function on the sub class.
+     */
+    private checkForGameOver(): void {
+        if (this.game.players.find((p) => p.won)) { // someone has won, so let's end this
+            this.isOver = true;
+
+            for (const player of this.game!.players) {
+                if (!player.won && !player.lost) { // then they are going to loose
+                    this.setPlayerLost(player, "Other player won");
+                }
+            }
+        }
+    }
+}
