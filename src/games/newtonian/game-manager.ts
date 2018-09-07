25316729bce86390fc655c1b272999bb246b9e80
JeffreyStrahm <jssfg8@mst.edu>
2018-09-07 13:50:21 -0500

    Added comments to guide game coding.

--- src/games/newtonian/game-manager.ts
+++ src/games/newtonian/game-manager.ts
@@ -57,6 +57,10 @@ export class NewtonianGameManager extends BaseClasses.GameManager {
 
         // <<-- Creer-Merge: after-turn -->>
         // add logic here after the current player's turn starts
+        // code spawning below this:
+        //
+        // code the generator below this:
+        //
         // <<-- /Creer-Merge: after-turn -->>
     }
 
