80f42ed3bac03d619f0290b1ed88a3733c8d797b
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-09-15 11:03:10 -0500

    Disable node_modules check on code that requires node_modules to run

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,15 @@
-/**
- * This file is the entry-point to start Cerveau
- */
+/** This file is the entry-point to start Cerveau */
+
+// first code to execute, like a mini sanity test
+console.log("~~~ Cerveau is starting ~~~"); // tslint:disable-line:no-console
+
 process.title = "Cerveau Game Server";
 
-// Check for npm install first, as many developers forget this pre step
-import { lstatSync } from "fs";
-
-try {
-    const lstat = lstatSync("./node_modules/");
-    if (!lstat.isDirectory()) {
-        throw new Error(); // to trigger catch below
-    }
-}
-catch (err) {
-    // tslint:disable-next-line no-console
-    console.error(`ERROR: "node_modules/" not found.
-    Did you forget to run 'npm install'?`);
-    process.exit(1);
-}
-
-// if we get here then the node_modules should be installed, let's go!
-
-// tslint:disable-next-line:no-import-side-effect - we want them below
 import { setupThread } from "./core/setup-thread";
-setupThread(); // we must do this before doing any aliased imports below
+setupThread(); // We must do this before doing any aliased imports below
 
 import { Lobby } from "./core/server";
-Lobby.getInstance(); // this will create the singleton Lobby instance
+Lobby.getInstance(); // This will create the singleton Lobby instance
 
 import { setupWebServer } from "./web/app";
 setupWebServer();
