623213fdd5957153439e37c649bef59a2c8a87ce
Cash <jjcfz2@mst.edu>
2018-09-26 02:31:30 -0500

    fixed all the tslinter issues. had a problem commiting with npm installed. Will look into it.

--- src/games/newtonian/game-manager.ts
+++ src/games/newtonian/game-manager.ts
@@ -253,7 +253,6 @@ export class NewtonianGameManager extends BaseClasses.GameManager {
                 spawnY = 1;
                 spawnX += playerIndex * -1;
             }
-
         }
         // Return failure. We finished looking over all the spawn for Unit spawning.
 
