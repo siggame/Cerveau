759eb6017c1743cf1cfae058383402eae8676004
Jack D <31171502+JackTildeD@users.noreply.github.com>
2019-03-08 17:12:46 -0600

    Added some code

--- src/games/stardash/unit.ts
+++ src/games/stardash/unit.ts
@@ -302,6 +302,10 @@ export class Unit extends GameObject {
         // body.materialType
         // body.amount
         
+        if (body.materialType == "genarium") {
+            this.genarium += body.amount;
+        }
+        
         return false;
 
         // <<-- /Creer-Merge: mine -->>
