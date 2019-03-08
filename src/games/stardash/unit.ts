1f7dbf66f55741241e3f82ed776781a42a8f5c2f
Jack D <31171502+JackTildeD@users.noreply.github.com>
2019-03-08 17:09:26 -0600

    Added comments

--- src/games/stardash/unit.ts
+++ src/games/stardash/unit.ts
@@ -248,7 +248,8 @@ export class Unit extends GameObject {
         // this.mythicite
         // this.rarium
         // miningSpeed
-        
+        // body.materialType
+        // body.amount
         
         return false;
 
