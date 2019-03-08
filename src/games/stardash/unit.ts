f969ab321bc5d342cb51d3550d9c669ab915be1a
Jack D <31171502+JackTildeD@users.noreply.github.com>
2019-03-08 16:47:15 -0600

    Added comments

--- src/games/stardash/unit.ts
+++ src/games/stardash/unit.ts
@@ -293,6 +293,11 @@ export class Unit extends GameObject {
         
         // Add ore to miner
         
+        // this.acted
+        // this.genarium
+        // this.legendarium
+        // this.mythicite
+        // this.rarium
         
         
         return false;
