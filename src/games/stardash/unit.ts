8cd44ee49504decf0de5ea1e0cdcab507a36cdb2
Jack D <31171502+JackTildeD@users.noreply.github.com>
2019-03-07 18:57:32 -0600

    Set astroid owener to player

--- src/games/stardash/unit.ts
+++ src/games/stardash/unit.ts
@@ -382,7 +382,7 @@ export class Unit extends GameObject {
         
         // Set astroid owener to player
         
-        
+        Body.owner = Player
         
         // Add ore to miner
         
