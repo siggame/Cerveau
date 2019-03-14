ddf7cb52b5fb8c608925f424cf3f5ecaf7d2ace7
Jack D <31171502+JackTildeD@users.noreply.github.com>
2019-03-14 17:40:59 -0500

    Update unit.ts

--- src/games/stardash/unit.ts
+++ src/games/stardash/unit.ts
@@ -261,9 +261,10 @@ export class Unit extends GameObject {
             return reason;
         }
         
-        if (this.acted) {
-            return `${this} has already acted!`
-        }
+        // Should already be checked
+        // if (this.acted) {
+        //     return `${this} has already acted!`
+        // }
         
         if (!Body) {
             return `Body doesn't exist`;
@@ -338,6 +339,7 @@ export class Unit extends GameObject {
         if (body.materialType == "rarium") {
             this.rarium += actualAmount;
         }
+        
         this.acted = true;
         
         return true;
