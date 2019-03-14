06c598168053bbd7db1cfbd01443b58734df188b
Jack D <31171502+JackTildeD@users.noreply.github.com>
2019-03-14 14:10:57 -0500

    Update unit.ts

--- src/games/stardash/unit.ts
+++ src/games/stardash/unit.ts
@@ -259,8 +259,10 @@ export class Unit extends GameObject {
 
         // check widely consistent things.
         const reason = this.invalidate(player, true);
-        
         // if there is a reason, return it.
+        if (reason) {
+            return reason;
+        }
         
         if (Body.BodyBodyType != "asteroid") {
             return `This body must be an asteroid!`  
