24c38906b448e441de14965527dbffc459a169c9
Strahm, Jeffrey S.(S&T-Student) <jssfg8@mst.edu>
2019-04-14 00:15:01 -0500

    Fixed small typo

--- src/games/stardash/body.ts
+++ src/games/stardash/body.ts
@@ -294,7 +294,7 @@ export class Body extends GameObject {
         // Slow solution; proposed: identify input job and check individual cost?
         // Unsure of how to implement above proposal
         if ((player.money < this.game.jobs[4].unitCost && title === "miner") || 
-            (player.money < this.game.jobs[3].unitCost && title === "transport")) ||
+            (player.money < this.game.jobs[3].unitCost && title === "transport") ||
             (player.money < this.game.jobs[0].unitCost && title === "corvette") ||
             (player.money < this.game.jobs[1].unitCost && title === "missileboat") ||
             (player.money < this.game.jobs[2].unitCost && title === "martyr")) {
