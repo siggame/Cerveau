19e5c0443be0ddfde23c2237b4eb2d02c94d1646
Jack D <31171502+JackTildeD@users.noreply.github.com>
2019-11-01 10:40:12 -0500

    Update tower.ts

--- src/games/necrowar/tower.ts
+++ src/games/necrowar/tower.ts
@@ -73,25 +73,6 @@ export class Tower extends GameObject {
     // in the creer file.
     
     // <<-- /Creer-Merge: public-functions -->>
-    
-    /*
-     * Returns the distance between the points
-     *
-     * @param x1: the first x coordinate.
-     * @param y1: the first y coordinate.
-     * @param x2: the second x coordinate.
-     * @param y2: the second y coordinate.
-     *
-     * @returns the distance between the points.
-     */
-    private distance(x1: number, y1: number, x2: number, y2: number): number {
-        // grab the differences.
-        const xDif: number = (x1 - x2);
-        const yDif: number = (y1 - y2);
-
-        // return the distance.
-        return Math.sqrt((xDif ** 2) + (yDif ** 2));
-    }
 
     /**
      * Invalidation function for attack. Try to find a reason why the passed in
@@ -115,7 +96,7 @@ export class Tower extends GameObject {
         // If you need to change an argument for the real function, then
         // changing its value in this scope is enough.
         
-        let range: number = 2.3;
+        let range: number = 2.3; // Attack range
         
         const reason = this.invalidate(player, true);
         // If there is a reason, return it.
@@ -127,6 +108,13 @@ export class Tower extends GameObject {
         if (this.attacked) {
             return
                 `${this}, cannot attack becuase has already attacked this turn`;
+        // Check if any unit belongs to the player
+        for (let i: number  = 0; i < tile.units.length; i++)
+        {
+            if (tile.units[i].owner === player)
+            {
+                return `${this}, cannot attack units on their own side`;
+            }
         }
         
         /// Check if tile exists
@@ -198,10 +186,12 @@ export class Tower extends GameObject {
             return `${this}, targets have zero health`;
         }
         
+        // Chick if tile has no units
         if (tile.units.length < 1) {
-            return `${this}, cannot attack because the tile has no units`;
+            return `${this}, cannot attack a tile with no units`;
         }
         
+        // Check if tower already attacked
         if (this.attacked) {
             return `${this}, has already attacked this turn`;
         }
@@ -215,27 +205,36 @@ export class Tower extends GameObject {
          *         _ x x x _
          */
         
+        // Check if tile is in range
         if (range < this.distance(this.tile.x, this.tile.y, tile.x , tile.y)) {
-            return `${this}, cannot attack because target is out of range`;
+            return `${this}, cannot attack because target tile is out of range`;
         }
         
+        // Check if type is valid
         if (!this.type) {
             return `${this}, has an unknown type`;
         }
         else
         {
             if (!this.type.title) {
-                return `${this}, has an unknown type`;
+                return `${this}, has an unknown type name`;
             }
         }
         
-        if (this.health < 1)
+        // Check if damage is valid
+        if (!this.damage) {
+            return `${this}, has unknown damage`;
+        }
+        
+        // Check if tower has zero health
+        if (this.health <= 0)
         {
             return `${this}, has zero health`;
         }
         
+        // Check if any unit on tile has health
         let found: boolean = false;
-        for (let i: number  = 0; i < tile.units.length; i++) {
+        for (let i: number = 0; i < tile.units.length; i++) {
             if (0 < this.tile.units[0].health)
             {
                 found = true;
@@ -243,7 +242,7 @@ export class Tower extends GameObject {
         }
         if (!found)
         {
-            return `${this}, target has zero health`;
+            return `${this}, targets have zero health`;
         }
         
         // <<-- /Creer-Merge: invalidate-attack -->>
@@ -258,8 +257,6 @@ export class Tower extends GameObject {
      */
     protected async attack(player: Player, tile: Tile): Promise<boolean> {
         // <<-- Creer-Merge: attack -->>
-
-        // Add logic here for attack.
         
         /*
          * Damage That Towers Do To Units
@@ -316,18 +313,21 @@ export class Tower extends GameObject {
                 game.units.splice(i, 1); // Remove unit from array
             }
         }
-
-        for (let i: number  = 0; i < tile.units.length; i++) {
-            if (tile.units[i].health < 1)
+        
+        // Remove units on tile with zero health and add corpses
+        for (let i: number = 0; i < tile.units.length; i++) {
+            if (tile.units[i].health <= 0)
             {
-                tile.corpses++;
-                tile.units.splice(i, 1);
+                tile.corpses++; // Add corpse to tile
+                tile.units.splice(i, 1); // Remove unit from array
             }
         }
-        for (let i: number  = 0; i < game.units.length; i++) {
-            if (tile.units[i].health < 1)
+
+        // Remove units on tile with zero health
+        for (let i: number = 0; i < game.units.length; i++) {
+            if (tile.units[i].health <= 0)
             {
-                game.units.splice(i, 1);
+                game.units.splice(i, 1); // Remove unit from array
             }
         }
         
@@ -355,5 +355,13 @@ export class Tower extends GameObject {
         return Math.sqrt((xDif ** 2) + (yDif ** 2));
     }
     
+        // grab the differences.
+        const xDif: number = (x1 - x2);
+        const yDif: number = (y1 - y2);
+
+        // return the distance.
+        return Math.sqrt((xDif ** 2) + (yDif ** 2));
+    }
+
     // <<-- /Creer-Merge: protected-private-functions -->>
 }
