66fde3aed6517ac389801f564c935e72f0ff31cc
Jack D <31171502+JackTildeD@users.noreply.github.com>
2020-04-08 13:58:40 -0500

    Update unit.ts

--- src/games/coreminer/unit.ts
+++ src/games/coreminer/unit.ts
@@ -320,19 +320,29 @@ export class Unit extends GameObject {
         }
         this.moves = Math.min(0, this.moves - 1);
         
-        // this.game.tiles[x + (y * this.game.mapWidth)]
-        let support = 0;
-        for (i = tile.x - 1; i >= tile.x + 1; i++) {
-            for (j = tile.y - 1; j >= tile.y - 4; j--) {
-                if (this.game.tiles[i + (j * this.game.mapWidth).isSupport) {
-                    support += 1;
-                }
-            }
+        if (!checkForSupport(tile)) {
+            tile.isFalling = true;
         }
         
         return false;
 
         // <<-- /Creer-Merge: mine -->>
+    }
+    
+    private checkForSupport(tile: Tile): boolean {
+        // this.game.tiles[x + (y * this.game.mapWidth)]
+        let support = 0;
+        for (i = tile.x - 1; i >= tile.x + 1; i++) {
+            for (j = tile.y - 1; j >= tile.y - 4; j--) {
+                if (this.game.tiles[i + (j * this.game.mapWidth)].isSupport) {
+                    support += 1;
+                }
+            }
+        }
+        if (0 < support) {
+            return true;
+        }
+        return false;
     }
 
     /**
